0a8088df24eed9146bb3df1d7ed822a698dbfed9
VSuveetha <126574613+VSuveetha@users.noreply.github.com>
2023-03-15 17:49:34 +0530

    contributing file included

---
+++ CONTRIBUTING.m
@@ -0,0 +1,3 @@
+Hi there! We're thrilled that you'd like to contribute to this project. Your help is essential for keeping it great.
+All contributions, bug reports, bug fixes, documentation improvements, enhancements, and ideas are welcome.
+Please note that this project is released to public under Apache licence 2.0.  
